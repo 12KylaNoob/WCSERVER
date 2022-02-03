d2a19d3aa937af224c02275e2772af672689d6a8
12KylaNoob <98504257+12KylaNoob@users.noreply.github.com>
2022-02-03 12:32:54 +0800

    Delete LabAct directory

--- LabAct/about.js
+++
@@ -1,7 +0,0 @@
-var name = require('./Name.js');
-var name2 = new name('John', 'Smith');
-module.exports = 'Hello ' + name2.fullName() + '. This activity will teach on how to deal with a simple server and local modules in Node.js';
-
-//ODVINA, Kyla Marie R.
-//February 2, 2022
-//WD-201
